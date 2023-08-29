5e0b5e57ed9fa77abe9c49cf0c68800ede9e98cd
Arturo Zepeda <aaze92@gmail.com>
2023-08-28 22:44:15 -0600

    añado validacion al formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive , ref } from 'vue';
+import Alerta from './Alerta.vue';
 const busqueda = reactive({
     ciudad: '',
     pais: ''
 });
+
+const error = ref('');
+
 const paises = [
     { codigo: 'US', nombre: 'Estados Unidos' },
     { codigo: 'MX', nombre: 'México' },
@@ -13,10 +17,19 @@ const paises = [
     { codigo: 'ES', nombre: 'España' },
     { codigo: 'PE', nombre: 'Perú' }
 ]
+
+const consultarClima = () =>{
+    if(Object.values(busqueda).includes('')){
+        error.value = 'Todos los campos son obligatorios';
+        return;
+    }
+    error.value = '';
+}
 </script>
 
 <template>
-    <form class="formulario">
+    <form class="formulario" @submit.prevent="consultarClima">
+        <Alerta v-if="error" :tipo="'error'">{{ error }}</Alerta>
         <div class="campo">
             <label for="ciudad">Ciudad</label>
             <input type="text" name="ciudad" id="ciudad" placeholder="Ciudad" v-model="busqueda.ciudad">
