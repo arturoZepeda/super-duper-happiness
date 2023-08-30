fb3e753b04dd3444434ec9e9d751a8b22dc91d8a
Arturo Zepeda <aaze92@gmail.com>
2023-08-29 21:35:32 -0600

    genero funcion consumiendo api para obtener el clima

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,12 +1,37 @@
+import axios from 'axios';
 export default function useClima() {
-   const obtenerClima = ({ciudad, pais}) => {
-        console.log("llego aqui")
-        console.log(ciudad);
-        console.log(pais);
-        /*const apikey = 'c7a361796fa1bec3e2002589f9f84b4c';
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apikey}&units=metric`;
+   const obtenerClima = async ({ciudad, pais}) => {
+        const apikey = import.meta.env.VITE_API_KEY;
+        /*
+        //Version 1
+        //genero consulta para obtener el latitud y longitud de la ciudad
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${apikey}`;
         const respuesta = await fetch(url);
-        const resultado = await respuesta.json();*/
+        const resultado = await respuesta.json();
+        //extraigo latitud y longitud
+        const {lat, lon} = resultado[0];
+        //genero consulta para obtener el clima
+        const url2 = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
+        const respuesta2 = await fetch(url2);
+        const resultado2 = await respuesta2.json();
+        return resultado2;
+        */
+        //Version 2 con axios
+        try{
+            //genero consulta para obtener el latitud y longitud de la ciudad con axios
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${apikey}`;
+            const respuesta = await axios.get(url);
+            //deestructuro latitud y longitud
+            const {lat, lon} = respuesta.data[0];
+            //genero consulta para obtener el clima
+            const url2 = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
+            //deestructuro data en resultado
+            const {data: resultado2} = await axios.get(url2);
+            return resultado2;
+        } catch (error) {
+            console.log(error);
+        }
+
    }
     
 
